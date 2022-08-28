LineageOS/android_packages_overlays_Lineage

overlays: Add black theme for dark mode [3/4]
Change-Id: I6bfa5a8d1a1ae31444acbbf60da942c4961d2171
Signed-off-by: Jesse Chan <jc@lineageos.org>

customizations/BlackTheme/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LineageBlackTheme
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LineageBlackTheme
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)

Nubianprince/android_packages_overlays_Leaf

customizations/BlackTheme/Android.mk
@@ -1,0 +1,12 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_RRO_THEME := LeafBlackTheme
+LOCAL_PRODUCT_MODULE := true
+LOCAL_CERTIFICATE := platform
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
+LOCAL_PACKAGE_NAME := LeafBlackTheme
+
+LOCAL_PRIVATE_PLATFORM_APIS := true
+
+include $(BUILD_RRO_PACKAGE)